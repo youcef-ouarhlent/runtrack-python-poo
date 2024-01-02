08bc111a514c3b17f165a115296d8a280337c6ba
youcef.ouarhlent <youcef.ouarhlent@laplateforme.io>
2024-01-02 10:36:04 +0100

    job03 terminé

---
+++ jour01/job03/job03.py
@@ -0,0 +1,9 @@
+class operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+    def addition(self):
+        return self.nombre1 + self.nombre2
+print(operation(2,2).addition()) 
+            
+ 

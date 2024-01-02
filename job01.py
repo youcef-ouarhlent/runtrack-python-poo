090e6aea3a19aea514e0c6077d017f17a1f0e60c
youcef.ouarhlent <youcef.ouarhlent@laplateforme.io>
2024-01-02 09:53:24 +0100

    Job01 terminé

---
+++ job01.py
@@ -0,0 +1,6 @@
+class Operation:#Une classe Operation
+    def __init__(self, number1, number2):#Une methode __init__ qui prend en parametre number1 et number2
+        self.number1 = number1#self.number1 est egale a number1
+        self.number2 = number2#self.number2 est egale a number2
+Operation(1, 2)#Operation prend en parametre 1 et 2
+print(Operation(1, 2))#Affiche <__main__.Operation object at 0x7f8b1c0b6a90>    

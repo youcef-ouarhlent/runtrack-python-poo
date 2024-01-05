38b9ba18e51d1fe938fe906bbadbbbc178afe8fc
youcef.ouarhlent <youcef.ouarhlent@laplateforme.io>
2024-01-05 09:56:48 +0100

    job02 terminé

---
+++ jour04/job02/job02.py
@@ -0,0 +1,43 @@
+class Personne:
+    def __init__(self, age=int(14)):
+        self.age = age
+
+    def afficherAge(self):
+        print("J'ai", self.age, "ans")
+
+    def bonjour(self):
+        print("Hello")
+
+    def modifierAge(self, age=int):
+        self.age = age    
+
+class Eleve(Personne):
+    def allerEnCours(self):
+        print("Je vais en cours")
+
+    def afficherAge(self):
+        print("J'ai", self.age, "ans")
+
+class Professeur(Personne):        
+    def __init__(self, matiereEnseignee):
+
+        self.__matiereEnseignee = matiereEnseignee
+
+    def enseigner(self):
+        print("Le cours va commencer")
+
+
+# Instanciation d'une classe Personne et d'une classe Eleve
+personne = Personne()
+eleve = Eleve()
+
+eleve.bonjour()
+eleve.allerEnCours()
+eleve.modifierAge(15)
+eleve.afficherAge()
+
+professeur = Professeur("Mathématiques")
+professeur.modifierAge(40)
+professeur.bonjour()
+professeur.enseigner()
+
